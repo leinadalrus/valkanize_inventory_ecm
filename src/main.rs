7738ea5244a2347d9eedea753056c37a3c23b3f6
Daniel David Sansano Surla <dandavesan@gmail.com>
2023-02-04 09:41:53 +1100

    updated some components

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContext, EguiPlugin};
+use bevy_kira_audio::prelude::*;
+use std::time::Duration;
 
 struct InteractionPanelAPI;
 struct TextPanelAPI;
@@ -32,7 +34,9 @@ pub struct EntityComponentInventory(InventoryData);
 
 impl EntityComponentInventory {
     fn new(self: Self) -> Self {
-        EntityComponentInventory(InventoryData { inventory_options: InventoryOptions::HeadIndex })
+        EntityComponentInventory(InventoryData {
+            inventory_options: InventoryOptions::HeadIndex,
+        })
     }
 }
 
@@ -45,12 +49,31 @@ fn contextualize_world_ui(mut egui_context: ResMut<EguiContext>) {
     });
 }
 
+fn serve_asset_resource_audio(asset_server: Res<AssetServer>, audio: Res<Audio>) {
+    audio
+        .play(asset_server.load("audio/music/background_elevator_music.flac"))
+        .loop_from(0.3)
+        // Fade-in with a dynamic easing
+        .fade_in(AudioTween::new(
+            Duration::from_secs(2),
+            AudioEasing::OutPowi(2),
+        ))
+        // Only play on our right ear
+        .with_panning(1.0)
+        // Increase playback rate by 50% (this also increases the pitch)
+        .with_playback_rate(1.5)
+        // Play at half volume
+        .with_volume(0.5);
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
+        .add_plugin(AudioPlugin)
         // Systems that create Egui widgets should be run during the `CoreStage::Update` stage,
         // or after the `EguiSystem::BeginFrame` system (which belongs to the `CoreStage::PreUpdate` stage).
         .add_system(contextualize_world_ui)
+        .add_system(serve_asset_resource_audio)
         .run();
 }
